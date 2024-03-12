94844f763b5eb1fd09b2e0633fe1d21744225615
Paul Le Gall <42955270+luapp@users.noreply.github.com>
2024-03-12 15:28:33 +0100

    Update App.js

--- example/src/App.js
+++ example/src/App.js
@@ -1,28 +1,31 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import app from './app.module.css'
 
 function App() {
-
-    const user_db = "luapp"
-    const password_db = "lx123"
+    const user_db = 'luapp'
+    const password_db = 'lx123'
     const [auth, setAuth] = useState(false)
 
 
-
-
+    
     const [user, setUser] = useState('')
     const [password, setPassword] = useState('')
+    const [todoList, setTodoList] = useState([])
+
+
+
+
 
     const handleUser = (e) => {
         setUser(e.target.value)
     }
+
     const handlePassword = (e) => {
         setPassword(e.target.value)
     }
 
-
     const login = () => {
-        if(user === user_db && password === password_db) {
+        if (user === user_db && password === password_db) {
             window.alert('Login Success')
             setAuth(true)
         } else {
@@ -31,28 +34,52 @@ function App() {
         }
     }
 
+    const handleAddTodo = () => {
+        const newTodo = prompt('Enter a new todo item:')
+        if (newTodo) {
+            setTodoList([...todoList, newTodo])
+        }
+    }
+
+    const handleRemoveTodo = (index) => {
+        const updatedList = [...todoList]
+        updatedList.splice(index, 1)
+        setTodoList(updatedList)
+    }
+
+
+
+
     if (auth) {
         return (
             <div className={app.app}>
                 <h1>Welcome {user}</h1>
                 <button onClick={() => setAuth(false)}>Logout</button>
+                <h2>Todo List:</h2>
+                <ul>
+                    {todoList.map((todo, index) => (
+                        <li key={index}>
+                            {todo}
+                            <button onClick={() => handleRemoveTodo(index)}>Remove</button>
+                        </li>
+                    ))}
+                </ul>
+                <button onClick={handleAddTodo}>Add Todo</button>
             </div>
         )
     } else {
         return (
             <div className={app.app}>
-              <h1>React Workshop Offline Auth</h1>
-              <br />
-              <input type="text" placeholder="Username" value={user} onChange={handleUser}/>
-              <br />
-              <input type="password" placeholder="Password" value={password} onChange={handlePassword} />
-              <br />
-              <button onClick={login}>Login</button>
+                <h1>React Workshop Offline Auth</h1>
+                <br />
+                <input type="text" placeholder="Username" value={user} onChange={handleUser} />
+                <br />
+                <input type="password" placeholder="Password" value={password} onChange={handlePassword} />
+                <br />
+                <button onClick={login}>Login</button>
             </div>
         )
     }
-
-    
 }
 
 export default App
