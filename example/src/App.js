ed51c5791e795a1cd3c9f2006b04391c6e29552c
Paul Le Gall <42955270+luapp@users.noreply.github.com>
2024-03-12 15:07:42 +0100

    Update App.js

--- example/src/App.js
+++ example/src/App.js
@@ -2,18 +2,57 @@ import { useState } from 'react'
 import app from './app.module.css'
 
 function App() {
-	const [counter, setCounter] = useState(0);
-	
-    return (
-      	<div className={app.app}>
-			<h1>React Workshop</h1>
-			<br />
-			<h2>Counter: {counter}</h2>
-			<br />
-			<button onClick={() => setCounter(counter + 1)}>+ Add to counter</button>
-			<button onClick={() => setCounter(counter - 1)}>- Subtract from counter</button>
-      	</div>
-    )
+
+    const user_db = "luapp"
+    const password_db = "lx123"
+    const [auth, setAuth] = useState(false)
+
+
+
+
+    const [user, setUser] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleUser = (e) => {
+        setUser(e.target.value)
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+
+    const login = () => {
+        if(user === user_db && password === password_db) {
+            window.alert('Login Success')
+            setAuth(true)
+        } else {
+            window.alert('Login Failed')
+            setAuth(false)
+        }
+    }
+
+    if (auth) {
+        return (
+            <div className={app.app}>
+                <h1>Welcome {user}</h1>
+                <button onClick={() => setAuth(false)}>Logout</button>
+            </div>
+        )
+    } else {
+        return (
+            <div className={app.app}>
+              <h1>React Workshop Offline Auth</h1>
+              <br />
+              <input type="text" placeholder="Username" value={user} onChange={handleUser}/>
+              <br />
+              <input type="password" placeholder="Password" value={password} onChange={handlePassword} />
+              <br />
+              <button onClick={login}>Login</button>
+            </div>
+        )
+    }
+
+    
 }
 
 export default App
